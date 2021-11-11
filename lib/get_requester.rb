9c346f8ba75ebbc866970631282180377880d396
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-11-11 10:59:48 -0500

    Completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
-# Write your code here
+require 'pry' 
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url) #parsing our url with URI.parse 
+        response = Net::HTTP.get_response(uri) #sends a get request to the url
+        response.body  #returns the body of the request as JSON STRING
+    end
+
+
+    def parse_json
+        JSON.parse(self.get_response_body) #converst JSON into ruby data structure
+    end
+end
+
+
+binding.pry
+
